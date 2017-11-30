67dd58b5ccea3c531071ff45242911b0532d5957
Matthieu Gilson <matthieu.gilson@upf.edu>
2017-11-30 03:15:34 +0100

    Add files via upload

---
+++ Classification.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 11:46:02 2017
+
+@author: matgilson
+"""
+
+import numpy as np
+import sklearn.linear_model as skllm
+import sklearn.neighbors as sklnn
+
+
+# load EC matrices, session labels and general parameters
+n_sub = 22
+n_run = 5
+N = 66
+
+EC = np.load('model_param/J_mod.npy')
+mask_EC = np.load('model_param/mask_EC.npy')
+
+vect_EC = EC[:,:,mask_EC] # vectorized EC matrices (only retaining existing connections)
+dim_feature = vect_EC.shape[2] # dimension of vectorized EC
+
+
+# labels of sessions for classification (train+test)
+sub_labels = np.repeat(np.arange(n_sub).reshape([-1,1]), n_run, axis=1)
+run_labels = np.repeat(np.arange(n_run).reshape([1,-1]), n_sub, axis=0)
+
+
+# classifier and learning parameters
+c_MLR = skllm.LogisticRegression(C=10000, penalty='l2', multi_class='multinomial', solver='lbfgs')
+c_1NN = sklnn.KNeighborsClassifier(n_neighbors=1, algorithm='brute', metric='correlation')
+
+n_rep = 6  # number of repetition of classification procedure
+perf = np.zeros([n_rep,2]) # record classification performance
+
+
+# perform classification
+for i_rep in range(n_rep):
+    # split run indices in train and test sets
+    run_train_labels = np.zeros([n_run],dtype=bool)
+    run_test_labels = np.ones([n_run],dtype=bool)
+    while run_train_labels.sum()<2:
+        rnd_int = np.random.randint(n_run)
+        if not run_train_labels[rnd_int]:
+            run_train_labels[rnd_int] = True
+            run_test_labels[rnd_int] = False
+    print('train/test sets:',run_train_labels,run_test_labels)
+    
+    # train and test classifiers with subject labels
+    c_MLR.fit(vect_EC[:,run_train_labels,:].reshape([-1,dim_feature]), sub_labels[:,run_train_labels].reshape([-1]))
+    perf[i_rep,0] = c_MLR.score(vect_EC[:,run_test_labels,:].reshape([-1,dim_feature]), sub_labels[:,run_test_labels].reshape([-1]))
+
+    c_1NN.fit(vect_EC[:,run_train_labels,:].reshape([-1,dim_feature]), sub_labels[:,run_train_labels].reshape([-1]))
+    perf[i_rep,1] = c_1NN.score(vect_EC[:,run_test_labels,:].reshape([-1,dim_feature]), sub_labels[:,run_test_labels].reshape([-1]))
+
+
+perf = np.array(perf) # change list in array
+print('average/std performance MLR',perf[:,0].mean(),perf[:,0].std())
+print('average/std performance 1NN',perf[:,1].mean(),perf[:,1].std())
+
